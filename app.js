034bd18f910a5c082aca0d172118ddd284c1c36d
Mustafa Oğuz BARAN <mustafaoguzbaran@gmail.com>
2022-09-21 12:13:32 +0300

    static files uploaded

--- app.js
+++ app.js
@@ -1,3 +1,10 @@
-const fn = function () {
-  console.log('Function Working');
-};
+const path = require('path')
+const express = require('express');
+const app = express();
+app.use(express.static('public'))
+const port = 3000;
+app.get("/", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "temp/index.html"))
+})
+app.listen(port, () => console.log(`Server listening on port ${port}`));
+
