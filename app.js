c7d7bab086e99122175d73dd4a8ac15f18dd5ac1
Mustafa Oğuz BARAN <mustafaoguzbaran@gmail.com>
2022-09-22 12:23:03 +0300

    project files updated.

--- app.js
+++ app.js
@@ -1,10 +1,21 @@
-const path = require('path')
+const path = require('path');
 const express = require('express');
+const ejs = require('ejs');
 const app = express();
-app.use(express.static('public'))
+//Template Engine
+app.set('view engine', 'ejs');
+//Middlewares
+app.use(express.static('public'));
 const port = 3000;
-app.get("/", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "temp/index.html"))
+//Routes
+app.get('/', (req, res) => {
+  // res.sendFile(path.resolve(__dirname, "temp/index.html"))
+  res.render('index');
+});
+app.get("/about", (req, res) => {
+    res.render('about');
+})
+app.get("/add", (req, res) => {
+    res.render('add')
 })
 app.listen(port, () => console.log(`Server listening on port ${port}`));
-
